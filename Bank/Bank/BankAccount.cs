99b67afe537c324c9b4f9991f9a89c6deb3fb058
RosGoodman <pavel.official@bk.ru>
2021-10-25 12:21:26 +0300

    add Task in readme, created sln. Task1 done.

---
+++ Bank/Bank/BankAccount.cs
@@ -0,0 +1,69 @@
+﻿
+namespace Bank
+{
+    class BankAccount
+    {
+        private readonly ulong _AccountNumb;
+        private decimal _Balance;
+        private readonly AccountType _AccountType;
+
+        #region properties
+
+        /// <summary> Номер банковского счета.</summary>
+        public ulong AccountNumb
+        {
+            get => _AccountNumb; 
+            init => _AccountNumb = value; 
+        }
+
+        /// <summary> Баланс банковского счета. </summary>
+        public decimal Balance { get; }
+
+        /// <summary>Тип банковского счета.</summary>
+        public AccountType AccountType
+        {
+            get => _AccountType;
+            init => _AccountType = value;
+        }
+
+        #endregion
+
+        /// <summary>Создать экземпляр класса BankAccount.</summary>
+        /// <param name="accNumb">Номер банковского счета.</param>
+        /// <param name="accType">Тип банковского счета.</param>
+        public BankAccount(ulong accNumb, AccountType accType)
+        {
+            AccountNumb = accNumb;
+            AccountType = accType;
+        }
+
+        #region AccountMethods
+
+        /// <summary>Положить сумму на счет.</summary>
+        /// <param name="sum">Сумма, которая будет добавлена на счет.</param>
+        public decimal AddSum(decimal sum)
+        {
+            _Balance += sum;
+            return _Balance;
+        }
+
+        /// <summary>Снять сумму со счета.</summary>
+        /// <param name="sum">Сумма, которая будет снята со счета.</param>
+        public decimal WithdrawSum(decimal sum) 
+        {
+            if(_Balance < sum) return _Balance;
+
+            _Balance -= sum;
+            return _Balance;
+        }
+
+        public override string ToString()
+        {
+            return $"AccNumber: {_AccountNumb}\n" +
+                $"balance: {_Balance}\n" +
+                $"accType: {_AccountType}\n";
+        }
+
+        #endregion
+    }
+}
---
+++ Bank/Bank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Bank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ulong accNumb = 13245678900987654321;
+            BankAccount account = new BankAccount(accNumb, AccountType.Current);
+
+            Console.WriteLine(account.ToString());
+
+            account.AddSum(123.4M);
+            Console.WriteLine(account.ToString());
+
+            account.WithdrawSum(100M);
+            Console.WriteLine(account.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
