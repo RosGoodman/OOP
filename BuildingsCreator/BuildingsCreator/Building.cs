9af33aeb64051d4df6cf7b572f863b443fdec0e6
RosGoodman <pavel.official@bk.ru>
2021-10-28 11:05:57 +0300

    task1 done

---
+++ BuildingsCreator/BuildingsCreator/Building.cs
@@ -0,0 +1,80 @@
+﻿
+namespace BuildingsCreator
+{
+    class Building
+    {
+        private static int _LastNumb;
+
+        public int _BuildingNumb;
+        private float _BuildingHeight;
+        private int _NumbOfFloors = 1;
+        private int _NumbOfApartment;
+        private int _NumbOfEntrances;
+
+        #region Properties
+
+        public int BuildingNumb
+        {
+            get => _BuildingNumb;
+            private set => _BuildingNumb = value;
+        }
+
+        public float BuildingHeight
+        {
+            get => _BuildingHeight;
+            private set => _BuildingHeight = value;
+        }
+
+        public int NumbOfFloors
+        {
+            get => _NumbOfFloors;
+            private set => _NumbOfFloors = value;
+        }
+
+        public int NumbOfApartment
+        {
+            get => _NumbOfApartment;
+            private set => _NumbOfApartment = value;
+        }
+
+        public int NumbOfEntrances
+        {
+            get => _NumbOfEntrances;
+            private set => _NumbOfEntrances = value;
+        }
+
+        #endregion
+
+        #region Ctors
+
+        public Building()
+        {
+            BuildingNumb = GenerateBuildNumb();
+        }
+
+        public Building(float bHeight, int floorsCount, int apartCount, int entrancesCount)
+        {
+            BuildingNumb = GenerateBuildNumb();
+            BuildingHeight = bHeight;
+            _NumbOfFloors = floorsCount;
+            _NumbOfApartment = apartCount;
+            _NumbOfEntrances = entrancesCount;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public float GetHeightOfFloor()
+        {
+            //отрицательные. Вдруг кто-то захочет жить под землей...¯\_(ツ)_/¯
+            if (_BuildingHeight != 0)
+                return _BuildingHeight / _NumbOfFloors;
+            else return 0;
+        }
+
+        public static int GenerateBuildNumb() => ++_LastNumb;
+
+        #endregion
+    }
+}
--- BuildingsCreator/BuildingsCreator/Program.cs
+++ BuildingsCreator/BuildingsCreator/Program.cs
@@ -6,7 +6,15 @@ namespace BuildingsCreator
     {
         static void Main(string[] args)
         {
-            
+            Building building1 = new Building(20f, 7, 200, 6);
+            Building building2 = new Building();
+
+            Console.WriteLine(building1.BuildingNumb);
+            Console.WriteLine(building2.BuildingNumb);
+
+            Console.WriteLine(building1.GetHeightOfFloor());
+
+            Console.ReadKey();
         }
     }
 }
