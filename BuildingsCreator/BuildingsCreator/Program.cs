cc897f85321c0306ce663de661f69188b4e294d0
Pavel <pavel.official@bk.ru>
2021-10-31 15:44:42 +0300

    ...

--- BuildingsCreator/BuildingsCreator/Program.cs
+++ BuildingsCreator/BuildingsCreator/Program.cs
@@ -1,36 +1,36 @@
 ﻿
-using BuildingsCreator.Fctory;
-using System;
+//using BuildingsCreator.Fctory;
+//using System;
 
 namespace BuildingsCreator
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            //создание и запись зданий.
-            Creator.CarteBuild(new ConcreteFactory(), 20, 7, 200, 6);
-            Creator.CarteBuild(new ConcreteFactory(), 25, 9, 230, 6);
-            var foundBuild = Creator.GetBuilding(1);
-            var foundBuild2 = Creator.GetBuilding(2);
+        //static void Main(string[] args)
+        //{
+        //    //создание и запись зданий.
+        //    Creator.CarteBuild(new ConcreteFactory(), 20, 7, 200, 6);
+        //    Creator.CarteBuild(new ConcreteFactory(), 25, 9, 230, 6);
+        //    var foundBuild = Creator.GetBuilding(1);
+        //    var foundBuild2 = Creator.GetBuilding(2);
 
-            //расчет высоты этажей.
-            Console.WriteLine($"Высота одного этажа: {foundBuild.GetHeightOfFloor()}");
-            Console.WriteLine($"Высота одного этажа: {foundBuild2.GetHeightOfFloor()}");
+        //    //расчет высоты этажей.
+        //    Console.WriteLine($"Высота одного этажа: {foundBuild.GetHeightOfFloor()}");
+        //    Console.WriteLine($"Высота одного этажа: {foundBuild2.GetHeightOfFloor()}");
 
-            //проверка удаления зданий из хэш-таблицы
-            Console.WriteLine($"Хэш-таблица содержит здание №1: {Creator.Contain(1)}");
-            Console.WriteLine("Удаление здания №1.");
+        //    //проверка удаления зданий из хэш-таблицы
+        //    Console.WriteLine($"Хэш-таблица содержит здание №1: {Creator.Contain(1)}");
+        //    Console.WriteLine("Удаление здания №1.");
 
-            Creator.RemoveBuilding(1);
+        //    Creator.RemoveBuilding(1);
 
-            foundBuild = Creator.GetBuilding(1);
-            foundBuild2 = Creator.GetBuilding(2);
+        //    foundBuild = Creator.GetBuilding(1);
+        //    foundBuild2 = Creator.GetBuilding(2);
 
-            Console.WriteLine($"Хэш-таблица содержит здание №1: {Creator.Contain(1)}");
-            Console.WriteLine($"Хэш-таблица содержит здание №2: {Creator.Contain(2)}");
+        //    Console.WriteLine($"Хэш-таблица содержит здание №1: {Creator.Contain(1)}");
+        //    Console.WriteLine($"Хэш-таблица содержит здание №2: {Creator.Contain(2)}");
 
-            Console.Read();
-        }
+        //    Console.Read();
+        //}
     }
 }
